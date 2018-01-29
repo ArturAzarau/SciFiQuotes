5168774865a021337cd7649d58233cf4b43a4d9f
arturazarov <artur.azarau@gmail.com>
2018-01-29 14:37:57 +0300

    Initial Commit

---
+++ SciFiQuotes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SciFiQuotes
+//
+//  Created by Артур Азаров on 29.01.2018.
+//  Copyright © 2018 Артур Азаров. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
